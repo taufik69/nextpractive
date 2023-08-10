fd1532b093a595190d2bbbcd844d3b0b87e938d8
taufik69 <taufik35-2915@diu.edu.bd>
2023-08-10 13:03:26 +0600

    data fetch via axios

--- src/app/moive/[slug]/page.jsx
+++ src/app/moive/[slug]/page.jsx
@@ -23,7 +23,7 @@ const moive1 = () => {
       };
       try {
         const response = await axios.request(options);
-        setfetchData(response.data);
+        setfetchData(response.data.titles);
       } catch (error) {
         console.error(error);
       }
@@ -32,8 +32,51 @@ const moive1 = () => {
   }, []);
   console.log("fetch data is : ", fetchData);
   return (
-    <div className="bg-black text-white font-3xl text-center p-16">
-      <h1>This is dynamic page 1</h1>
+    <div className="flex justify-center items-center flex-wrap  bg-black p-5 gap-7">
+      {fetchData.map((item) => (
+        <div className="">
+          <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <a href="#">
+              <img
+                class="rounded-t-lg"
+                src={item.jawSummary.backgroundImage.url}
+                alt=""
+              />
+            </a>
+            <div class="p-5">
+              <a href="#">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  {item.jawSummary.title}
+                </h5>
+              </a>
+              <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                {item.jawSummary.currentContextualSynopsis.text}
+              </p>
+              <a
+                href="#"
+                class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Read more
+                <svg
+                  class="w-3.5 h-3.5 ml-2"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 14 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M1 5h12m0 0L9 1m4 4L9 9"
+                  />
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
--- src/app/moive/page.jsx
+++ src/app/moive/page.jsx
@@ -1,13 +1,16 @@
-"use client";
 import React from "react";
 import Link from "next/link";
-
 const Moive = () => {
   return (
     <div className="bg-black text-white font-3xl text-center p-16">
       <h1>This is moive page</h1>
-      <Link href={"/moive/dynamic"}>
-        <button className="bg-red-600 px-6 py-2 mt-5">see more</button>
+      <Link href={"/moive/2"}>
+        <button
+          type="button"
+          class="w-[30%] mt-5 text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2"
+        >
+          Purple
+        </button>
       </Link>
     </div>
   );
